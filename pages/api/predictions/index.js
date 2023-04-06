e96bf5444ff382b5acdcd6576e3f9b1d2e477a38
afarhadi99 <afarhadi@mytsi.org>
2023-04-05 23:00:05 -0400

    main

---
+++ pages/api/predictions/index.js
@@ -0,0 +1,31 @@
+import Replicate from "replicate";
+
+const replicate = new Replicate({
+  auth: process.env.REPLICATE_API_TOKEN,
+});
+
+export default async function handler(req, res) {
+  if (!process.env.REPLICATE_API_TOKEN) {
+    throw new Error(
+      "The REPLICATE_API_TOKEN environment variable is not set. See README.md for instructions on how to set it."
+    );
+  }
+
+  const prediction = await replicate.predictions.create({
+    // Pinned to a specific version of Stable Diffusion
+    // See https://replicate.com/stability-ai/stable-diffussion/versions
+    version: "9936c2001faa2194a261c01381f90e65261879985476014a0a37a334593a05eb",
+
+    // This is the text prompt that will be submitted by a form on the frontend
+    input: { prompt: req.body.prompt },
+  });
+
+  if (prediction?.error) {
+    res.statusCode = 500;
+    res.end(JSON.stringify({ detail: prediction.error }));
+    return;
+  }
+
+  res.statusCode = 201;
+  res.end(JSON.stringify(prediction));
+}
